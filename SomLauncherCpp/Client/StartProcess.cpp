efb03b15b6d2014146df45d0ee3a505aa70c794e
NiNeDelixe <alkor.sidius@gmail.com>
2024-02-24 00:44:00 +0300

    fix output in process

--- SomLauncherCpp/Client/StartProcess.cpp
+++ SomLauncherCpp/Client/StartProcess.cpp
@@ -25,32 +25,42 @@ int client::utils::doProcess(std::unique_ptr<wchar_t[]>& buffer)
 	sa.lpSecurityDescriptor = NULL;
 	sa.bInheritHandle = TRUE;
 
-	HANDLE h = CreateFile(L"somlogs\\last_minecraft_out.log",
-		FILE_APPEND_DATA,
-		FILE_SHARE_WRITE | FILE_SHARE_READ,
-		&sa,
-		OPEN_ALWAYS,
-		FILE_ATTRIBUTE_NORMAL,
-		NULL);
+	HANDLE h = CreateFile(L"somlogs\\last_minecraft_out.txt",
+		GENERIC_WRITE, 0, &sa, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
 
-
-	STARTUPINFO si;
-	//memset(&si, 0, sizeof(si));
-	ZeroMemory(&si, sizeof(STARTUPINFO));
-	si.cb = sizeof(si);
-
-	DWORD flags = CREATE_NO_WINDOW;
-
-	si.dwFlags = STARTF_USESTDHANDLES;
-	//si.hStdInput = h;
-	si.hStdError = h;
-	si.hStdOutput = h;
+	if (h == INVALID_HANDLE_VALUE)
+	{
+		qWarning() << "error create mc log file" << std::endl;
+	}
 
 	PROCESS_INFORMATION pi;
+	STARTUPINFOW si;
+
+	ZeroMemory(&si, sizeof(si));
 	memset(&pi, 0, sizeof(pi));
 
+	si.cb = sizeof(STARTUPINFOW);
+	si.hStdError = h; // Перенаправляем стандартный поток ошибок в файл
+	si.hStdOutput = h; // Перенаправляем стандартный поток вывода в файл
+	si.dwFlags |= STARTF_USESTDHANDLES; // Устанавливаем флаг, чтобы использовать перенаправление потоко
+
+	//STARTUPINFO si;
+	////memset(&si, 0, sizeof(si));
+	//ZeroMemory(&si, sizeof(STARTUPINFO));
+	//si.cb = sizeof(si);
+
+	//DWORD flags = CREATE_NO_WINDOW;
+
+	//si.dwFlags = STARTF_USESTDHANDLES;
+	////si.hStdInput = h;
+	//si.hStdError = h;
+	//si.hStdOutput = h;
+
+	//PROCESS_INFORMATION pi;
+	//memset(&pi, 0, sizeof(pi));
+
 	qInfo() << "Programm args setting complete" << std::endl;
-	if (CreateProcessW(NULL, buffer.get(), NULL, NULL, TRUE, flags, NULL, NULL, &si, &pi))
+	if (CreateProcessW(NULL, buffer.get(), NULL, NULL, TRUE, NULL, NULL, NULL, &si, &pi))
 	{
 		// программа запущена, ждем её завершения
 		qInfo() << "Programm has been started" << std::endl;
